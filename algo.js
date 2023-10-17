7fa05cec43146eab8a4f971740188ea869c2592c
antoine <antoine.drionduchapoi@student.vinci.be>
2023-10-17 17:44:58 +0200

    value volume, month, day

--- algo.js
+++ algo.js
@@ -1,12 +1,40 @@
 const scrapeData = require('./scraping');
 const yahooStockAPI = require('yahoo-stock-api').default;
 
+async function main() {
+    const index = await scrapeData.index();
+    const dump = await scrapeData.dump();
+    const sentiment = await scrapeData.sentiment();
+    const tradingVolume = await scrapeData.tradingVolume();
+    const messageVolume = await scrapeData.messageVolume();
+    const dayMonth = await scrapeData.dayMonth();
+    const dayWeek = await scrapeData.dayWeek();
+
+
+    const result = processData(index, dump, sentiment, tradingVolume, messageVolume, dayMonth, dayWeek);
+    console.log(index + " " + result.decision + " " + result.value);
+    //await tradingAlgorithm();
+}
+
+
+function processData(index, dump, sentiment, tradingVolume, messageVolume, dayMonth, dayWeek) {
+
+    const value = dump + sentiment + tradingVolume + messageVolume + dayMonth + dayWeek;
+
+    let decision = "PASS";
+    if (value >= 4) {
+        decision = "BUY";
+    }
+
+    return {decision, value};
+}
+
 async function tradingAlgorithm() {
-    const symbol = await scrapeData();
+    const symbol = await scrapeData.index();
     // Implement your trading algorithm logic here
     const yahoo = new yahooStockAPI();
-    const startDate = new Date('08/21/2020');
-    const endDate = new Date('08/26/2020');
+    const startDate = new Date();
+    const endDate = new Date();
 
 
     const response = await yahoo.getHistoricalPrices({startDate, endDate, symbol, frequency: '1d',});
@@ -17,8 +45,8 @@ async function tradingAlgorithm() {
 }
 
 
-tradingAlgorithm();
 
+main();
 module.exports = tradingAlgorithm;
 
 
--- scraping.js
+++ scraping.js
@@ -1,30 +1,74 @@
 const puppeteer = require('puppeteer');
+const {default: yahooStockAPI} = require("yahoo-stock-api");
 
-async function scrapeData() {
+async function index() {
     //Regarde sur Stocktwits stock à prendre
-    return 'TTOO';
+    return 'AMZN';
 }
 
 async function dump() {
     //Regarde si dump à l'ouverture
     // fait appel à l'api de yahoo et va regarder si prix à l'ouverture plus bas qu'à 9h50
-    return true;
+    return 1;
 }
 
 async function sentiment() {
-    //Regarde si quel est le sentiment sur Stocktwits
-    return true;
+    //Regarde quel est le sentiment sur Stocktwits
+    return 1;
 }
 
 async function tradingVolume() {
-    //Regarde trading volume
-    // utilise puppeteer mais sur yahooFinance
-    return true;
+    // Look for trading volume
+    const symbol = await index();
+    const yahoo = new yahooStockAPI();
+    const startDate = new Date();
+    const endDate = new Date();
+    const response = await yahoo.getHistoricalPrices({startDate, endDate, symbol, frequency: '1d',});
+
+    // If tradingVolume > then 100000 then value +1
+    const tradingVolume = response.response[0].volume;
+    let tradingVolumeValue = 0;
+    if (tradingVolume > 2000000){
+        tradingVolumeValue = 1;
+    }
+
+
+    return tradingVolumeValue;
 }
 
 async function messageVolume() {
     //Regarde message volume
-    return true;
+    return 1;
 }
 
-module.exports = scrapeData, dump, sentiment, tradingVolume, messageVolume;
+async function dayMonth() {
+    //Regarde jour du mois => début du mois mieux
+    const today = new Date().getDate();
+    let dayMonthValue = 0;
+    if (today < 10){
+        dayMonthValue = 0.5;
+    }
+
+    return dayMonthValue;
+}
+
+async function dayWeek() {
+    //Regarde jour de la semaine => vdd dangereux, lundi bien
+    const today = new Date().getDay();
+
+    let dayWeekValue = 0;
+    if (today > 4){
+        dayWeekValue = -0.5;
+    }
+    return dayWeekValue;
+}
+
+module.exports = {
+    index,
+    dump,
+    sentiment,
+    tradingVolume,
+    messageVolume,
+    dayMonth,
+    dayWeek,
+};
